115735842aeee1b5b358eefcc7d3c63a3ac77e23
GermanPotato <lara@mueller-kl.de>
2024-10-04 14:18:34 +0200

    Adding Enemy

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D enemyrb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyrb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Time.deltaTime, 0, 0);
+    }
+}
